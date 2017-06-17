588b77becc40a4180f81ae09acc0ce71a4e484d6
Ethan Rogers <erogers6264@gmail.com>
2017-06-16 21:33:03 -0700

    Generate random colors for each square

---
+++ notSquare.js
@@ -0,0 +1,14 @@
+var squares = document.querySelectorAll(".square");
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+squares.forEach(function(square) {
+  var red = getRandomInt(0, 256);
+  var green = getRandomInt(0, 256);
+  var blue = getRandomInt(0, 256);
+  square.style.background = "rgb(" + red + ", " + green + ", " + blue + ")";
+})
