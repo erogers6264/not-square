b7a7a3e8387ebe1c8a8f636de897c1320ec33bae
Ethan Rogers <erogers6264@gmail.com>
2017-06-18 02:48:53 -0700

    Pick color and add checking logic

--- notSquare.js
+++ notSquare.js
@@ -1,6 +1,17 @@
 
 var squares = document.querySelectorAll(".square");
 
+var colors = [];
+for (var i = squares.length - 1; i >= 0; i--) {
+  colors.push(getRandomRGB());
+}
+
+var pickedColor = colors[getRandomInt(0, colors.length + 1)];
+var h1 = document.querySelector('h1');
+var t = document.createTextNode(' ' + pickedColor);
+h1.appendChild(t);
+
+
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -17,6 +28,16 @@ function getRandomRGB() {
   return rgb;
 }
 
+var colorCounter = 0;
 squares.forEach(function(square) {
-  square.style.backgroundColor = getRandomRGB();
+  square.style.backgroundColor = colors[colorCounter];
+  colorCounter++;
+  square.addEventListener('click', function() {
+    if (square.style.backgroundColor === pickedColor) {
+      alert("You're right!!")
+    } else {
+      alert("WRONG!")
+      square.style.backgroundColor = '#232323';
+    }
+  })
 });
